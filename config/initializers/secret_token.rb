88879420b330b891253a336a9cfcee6833918f49
rails new alancitus <agarcia@usil.edu.pe>
2015-06-01 16:26:10 -0500

    Creando el proyecto y agregando status al mismo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Social::Application.config.secret_token = 'badb9a880bc8d522d2081a611c872133467915e510af6dc5495eef4a5eaf60e1c250836ce13bc02531503bee43e806fc367accfcab9c63960f6780c503d72582'
